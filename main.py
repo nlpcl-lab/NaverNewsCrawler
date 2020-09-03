8feedc176817e2eba491c9664fd91d6927f23ef0
huijelee <jae4258@naver.com>
2020-09-03 14:01:39 +0900

    Initialization

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 from korea_news_crawler.articlecrawler2 import ArticleCrawler
 
 TARGET = '경제'
-SUB_TARGET = '증권 금융 부동산 산업재계 글로벌경제 경제일반 생활경제 증기벤처'
+SUB_TARGET = '증권 금융 부동산' # 산업재계 글로벌경제 경제일반 생활경제 증기벤처'
 
 Crawler = ArticleCrawler()  
 Crawler.set_category(*tuple(re.split(' ', TARGET)), subcategories=re.split(' ', SUB_TARGET))
