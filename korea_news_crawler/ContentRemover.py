aa2b6284a929b0efb1aec8c3741f969cc6c1855a
huijelee <jae4258@naver.com>
2020-10-08 05:12:38 +0900

    fix news_cralwer by internal error (avoiding python crawler rejection)

--- korea_news_crawler/ContentRemover.py
+++ korea_news_crawler/ContentRemover.py
@@ -1,10 +1,12 @@
 ################################################################
 # 2020.09.08 ContentRemover.py
-# 신한 Naver news data에서 title과 body text를 전처리 합니다.
+# input text (신한 Naver news data)에서 title과 body text를 전처리 합니다.
 #   - 기자 이름, 출판사 광고, 관련 뉴스 문구 삭제
 #   - 영어, 한문, 한글 외 언어, 특수 문자 제거
 #   - 인터넷 매크로 문구, 불필요한 유니코드 제거
 #   - 중복/빈 기사 삭제 (필터링 후 제목 일치 여부 기준)
+# 실행 예제: python ContentRemover.py --input <input directory> --output preprocessed --input-delimeter \t --input-fieldnames link-date-publisher-thumbnail-title-body
+# python ContentRemover.py --input D:/dataset/nnc --output nnc_preprocessed --input-delimeter , --input-fieldnames date-category-subcategory-publisher-title-body-link
 ################################################################
 # -*- coding: utf-8, euc-kr -*-
 import os
@@ -17,15 +19,19 @@ from glob import glob
 from collections import OrderedDict
 from korea_news_crawler.articleparser import ArticleParser
 
-fieldnames = ['link', 'date', 'publisher', 'thumbnail', 'title', 'body']
 
 def main():
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=__doc__)
     parser.add_argument("input", help="input directory including text files")
+    parser.add_argument("input-delimeter", default='\t', help="a delimiter which the input text contains")
+    parser.add_argument("input-fieldnames", default='link-date-publisher-thumbnail-title-body', help="a list of column names")
     parser.add_argument("output", help="new directory name (not pathway)")
     args = parser.parse_args()
+    fieldnames = re.split('-', args.input_fieldnames)
+    title_idx = fieldnames.index('title')
+    body_idx = fieldnames.index('body')
 
     # 1. list naver news list (glob)
     fnames = glob(args.input + '/*')
@@ -52,22 +58,32 @@ def main():
                     initial_news_num = len(lines)
                     lines = [x for x in lines if x]
                     for line in lines:
-                        tokens = re.split('\t', line)
-                        if len(tokens) != 6:    # 형식에 벗어난 기사 예외 처리
+                        tokens = re.split(args.input_delimeter, line)
+                        if len(tokens) != len(fieldnames):    # 형식에 벗어난 기사 예외 처리
                             continue
                         # 3. clear naver news text
-                        title4 = ArticleParser.clear_headline(tokens[4])
-                        if not title4:  # 공백일 경우 기사 제외 처리
+                        title = ArticleParser.clear_headline(tokens[title_idx])
+                        if not title:  # 공백일 경우 기사 제외 처리
                             continue
-                        body5 = ArticleParser.clear_content(tokens[5])
-                        if not body5:  # 공백일 경우 기사 제외 처리
+                        body = ArticleParser.clear_content(tokens[body_idx])
+                        if not body:  # 공백일 경우 기사 제외 처리
                             continue
-                        if tokens[4] in news_dict:
+                        if tokens[title_idx] in news_dict:
                             duplicated_num += 1
                             TOTAL_DUPLICATED_NUM += 1
-                        news_dict[tokens[4]] = '\t'.join(tokens[:4] + [title4] + [body5]) + '\n'
 
-                    # 4. write clean news texts
+                        # 4. save clean text in dict
+                        result = []
+                        for i, token in enumerate(tokens):
+                            if i == title_idx:
+                                result.append(title)
+                            elif i == body_idx:
+                                result.append(body)
+                            else:
+                                result.append(token)
+                        news_dict[tokens[title_idx]] = args.input_delimeter.join(result) + '\n'
+
+                    # 5. write clean news texts
                     after_total_news_num = len(news_dict)
                     for _, text in news_dict.items():
                         fout.write(text)
@@ -80,27 +96,6 @@ def main():
 
         print("Total duplicated news num: {}".format(TOTAL_DUPLICATED_NUM))
         logwriter.write("Total duplicated news num: {}".format(TOTAL_DUPLICATED_NUM))
-
-        # 중복 기사 제거 기능 없음, 빠른 처리속도
-        """
-        for fname in fnames:
-            with codecs.open(fname, 'r', encoding='utf-8', errors='ignore') as fin:
-                with codecs.open("{}/{}/{}".format('/'.join(re.split('/', args.input)[:-1]), args.output, re.split('[/\\\\]', fname)[-1]), 'w', encoding='utf-8', errors='ignore') as fout:
-                    text = fin.read()
-                    lines = re.split('[\r\n]+', text)
-                    lines = [x for x in lines if x]
-                    for line in lines:
-                        tokens = re.split('\t', line)
-                        # 3. clear naver news text
-                        title4 = ArticleParser.clear_headline(tokens[4])
-                        if not title4:  # 공백일 경우 기사 제외 처리
-                            continue
-                        body5 = ArticleParser.clear_content(tokens[5])
-                        if not body5:  # 공백일 경우 기사 제외 처리
-                            continue
-                        # 4. write clean news texts
-                        fout.write('\t'.join(tokens[:4] + [title4] + [body5]) + '\n')
-        """
 
 
 if __name__ == "__main__":
